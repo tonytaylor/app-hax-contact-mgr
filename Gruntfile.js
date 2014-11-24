d1270e24d511a0508b388ff8fcb3b770c088e47f
Tony Taylor <tony@tonetaylor.com>
2014-11-23 23:07:39 -0600

    added control-bar and contact-mgr components

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/* jshint node:true */
+module.exports = function(grunt) {
+	
+	grunt.loadNpmTasks('web-component-tester');
+
+	grunt.registerTask('default', function() {
+		console.log('more coming soon');
+	});
+};
